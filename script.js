b685859a28eb4dd493ebffb6e2910329f194ab88
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-05-17 18:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	let str = s.trim();
+	let spaceIndex = str.indexOf(' ');
+	if(spaceIndex==-1 || str=='')return str;
+	else return str.substring(0,spaceIndex);
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
